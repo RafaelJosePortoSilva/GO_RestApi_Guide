a4c5fb701b6d10e497e8ba7d0f8d6aa9c23f92bf
RafaelJosePortoSilva <rafaelportot@proton.me>
2024-08-22 10:46:52 -0300

    main com route basico em net/http

---
+++ src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Define routes n handler functions
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	// Start server on :8080
+	port := 8080
+	fmt.Printf("Server running at //localhost:%d\n", port)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the homepage!!!")
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!!")
+}
